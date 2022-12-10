72248c48be222c5d10ec52fc3613b746872ac687
Sara-95-Sara <sara.galstyan.95@gmail.com>
2022-12-10 02:14:52 +0100

    pinta 5  imagenes usando la propiedad innerHtml

--- DOM/index.js
+++ DOM/index.js
@@ -1,6 +1,6 @@
 
 
-// funcion que pinta una imagen aleatoria
+// funcion que pinta una imagen aleatoria usando appendChild
 function drawRandomImage() {
    // creación de nodo
    const newImage = document.createElement('img');
@@ -13,6 +13,24 @@ function drawRandomImage() {
    target.appendChild(newImage);
 }
 
+
+// funcion que pinta 5 imagenes usando innerHtml
+function drawRandomImagesUsingInnerHtml() {
+
+    let images = "";
+
+    // generamos un string que contiene 5 etuiquetas img.
+    for (let index = 0; index < 5; index++) {
+        images = images + "<img src = 'https://picsum.photos/seed/picsum/200/300' ></img>"; 
+    }
+
+    // selecion del nodo destino
+    const target = document.querySelector('.images-wrapper');
+
+    // cambiamos todo el contenido de este nodo por lo que contiene la variable images.
+    target.innerHTML = images;
+}
+
 // borramos todas las imagenes cuando pasen 10 segundos.
 function removeAllImages() {
     const target = document.querySelector('.images-wrapper');
@@ -22,17 +40,26 @@ function removeAllImages() {
     childrenArray.forEach(img => img.remove());
 }
 
-// pintamos 5 imagenes cuando pasen 5 segundos
+
+
+// pintamos 5 imagenes cuando pasen 5 segundos  usando la propiedad innerHTML
+setTimeout( () => {
+    drawRandomImagesUsingInnerHtml();
+}, 5000);
+
+/*
+// pintamos 5 imagenes cuando pasen 5 segundos  usando el metodo appendChild
 setTimeout( () => {
     for (let index = 0; index < 5; index++) {
         drawRandomImage();
     }
 }, 5000);
 
+
 setTimeout(() => {
     removeAllImages();
 }, 8000);
-
+*/
 /*
 // ejecutar la funcion n veces, una vez cada x tiempo. cada 2.5 segundo aparece nuevo imagen.
 const interval = setInterval(() => {
